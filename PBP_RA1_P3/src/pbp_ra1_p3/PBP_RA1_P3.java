37615f2495d4e864b10f4e2e9d984c5e8cf62f90
pauborrell07 <pborrell.lsgi@gmail.com>
2025-10-15 09:20:03 +0200

    Commit 1 - 15/10/2025: Afegir dades a xml

--- PBP_RA1_P3/src/pbp_ra1_p3/PBP_RA1_P3.java
+++ PBP_RA1_P3/src/pbp_ra1_p3/PBP_RA1_P3.java
@@ -15,7 +15,7 @@ public class PBP_RA1_P3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
